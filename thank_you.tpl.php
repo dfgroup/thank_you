8cf80ecc51942331309a0d670270aac218b1dcd7
hatuhay <alberto@siles.pe>
2017-01-12 10:08:15 -0500

    Correct initial code
    Several improvements to code, language, indentation, pass logic to code
    rather than templates

--- thank_you.tpl.php
+++ thank_you.tpl.php
@@ -1,40 +1,7 @@
-<?php
-
-   $titulo = variable_get('pagina_gracias_titulo','');
-   $body = variable_get('pagina_gracias_body','');
-   $etiqueta_volver = variable_get('pagina_gracias_etiqueta_volver','');
-   $script = variable_get('pagina_gracias_script','');
-    
-
-$url = implode("/",$data);
- drupal_add_js($script, array(
- 	'type' => 'inline', 
- 	'scope' => 'header', 
- 	'weight' => 5)
-  );
-
-?> 
-
-
-  <aside  class="custom-page-gracias">
-
-<div class="titular-web"><h2><span><?php print $titulo;?></span></h2></div>
-
-<div class="white">
-  <div class="sumilla">
-    <p><img src="/sites/default/files/icon_error.png" alt="">&nbsp;<em><strong>Gracias </strong>Hemos recibido tu mensaje. Nos contactaremos a la brevedad posible contigo.</em>
-    
-      <span><a href="<?php  print "/".$url ; ?>"><?php print $etiqueta_volver;?></a></span>
-      
-      </p>
-</div>
-
-</div>
-
-  <!--		<div class="page-gracias-body">
-  		<p><?php print $body;?></p>
-  		</div> -->
-
-
-  		 
+<aside  class="thank-you-page">
+  <div class="title"><h2><span><?php print $title ?></span></h2></div>
+  <div class="content">
+    <?php print $body?>
+    <span><a href="<?php  print $url ?>"><?php print $return_label ?></a></span>
+  </div>
 </aside>
